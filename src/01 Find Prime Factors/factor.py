5ae7cb708f6de0b29bbdf9f50c856543e10771f3
manjunath8027 <manjunath8027@gmail.com>
2024-07-25 08:19:11 +0000

    modified the empty space

--- src/01 Find Prime Factors/factor.py
+++ src/01 Find Prime Factors/factor.py
@@ -10,6 +10,8 @@ def get_prime_factors(number):
     return factors
 
 
+
+
 # commands used in solution video for reference
 if __name__ == '__main__':
     print(get_prime_factors(630))  # [2, 3, 3, 5, 7]
